be856bbd58ca5656f67eb7db2a875ca8c852c753
tofiqaskerov <tofiqaskerov71@gmail.com>
2023-12-03 02:41:26 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import './App.css'
+
+type TPoint = {
+  x:number,
+  y:number
+}
+function App() {
+
+  const [points, setPoint] = useState<TPoint[]>([])
+  const [popped, setPopped] = useState<TPoint[]>([])
+
+  function handleUndo(){
+    const newPoints = [...points]
+    const poppedPoints = newPoints.pop()
+    if(!poppedPoints) return
+    setPopped([...popped, poppedPoints])
+    setPoint(newPoints)
+  }
+
+  function handleRedo(){
+    const newPopped = [...popped]
+    const lastPopped = newPopped.pop()
+    if(!lastPopped) return
+    setPopped(newPopped)
+    setPoint([...points, lastPopped])
+    
+  }
+  
+  function handlePlaceCircle(e: React.MouseEvent<HTMLDivElement>){
+    const {pageX, pageY} = e
+    setPoint([
+      ...points,
+       {x:pageX, y:pageY}
+      ])
+    
+  }
+  return <>
+  <button className="undo" onClick={handleUndo}>Undo</button>
+  <button  disabled={popped.length === 0 } className="redo" onClick={handleRedo}>Redo</button>
+  <div className='App'
+    onClick={handlePlaceCircle}
+  >
+    {
+      points.map((item, index) => <div key={index} className='point' style={{
+        left: item.x - 5+ "px",
+        top: item.y - 5+"px"
+      }}
+      >
+        
+      </div>)
+    }
+  </div>
+  </>
+}
+
+export default App
